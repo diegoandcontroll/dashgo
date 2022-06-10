2c0c11b5abfacf512145149a52660fe8c8bf3128
diegoandcontroll <lukasxdp2@gmail.com>
2022-06-10 20:33:44 -0300

    feat: create component pagination

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -41,28 +41,28 @@ export function makeServer() {
     },
 
     seeds(server) {
-      server.createList("user", 10);
+      server.createList("user", 200);
     },
 
     routes() {
       this.namespace = "api";
       this.timing = 750;
-      this.get("/users");
-      // this.get("/users", function (schema, req) {
-      //   const { page = 1, per_page = 10 } = req.queryParams;
+      //this.get("/users");
+      this.get("/users", function (schema, req) {
+        const { page = 1, per_page = 10 } = req.queryParams;
 
-      //   const total = schema.all("user").length;
+        const total = schema.all("user").length;
 
-      //   const pageStart = (Number(page) - 1) * Number(per_page);
-      //   const pageEnd = pageStart + Number(per_page);
+        const pageStart = (Number(page) - 1) * Number(per_page);
+        const pageEnd = pageStart + Number(per_page);
 
-      //   const users = this.serialize(schema.all("user")).users.slice(
-      //     pageStart,
-      //     pageEnd
-      //   );
+        const users = this.serialize(schema.all("user")).users.slice(
+          pageStart,
+          pageEnd
+        );
 
-      //   return new Response(200, { "x-total-count": String(total) }, { users });
-      // });
+        return new Response(200, { "x-total-count": String(total) }, { users });
+      });
 
       this.get("/users/:id");
       this.post("/users");
